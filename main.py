99c0172e88ab606a4747da0982d54ad9e77e2ec7
Admin <kimhieu280@gmail.com>
2024-05-10 14:38:29 +0700

    adđ little

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ pygame.init()
 
 WHITE = (255, 255, 255)
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 500
+SCREEN_HEIGHT = 760
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Inflation_RPG")
 
 bg = pygame.image.load("picture/background.png")
 char = pygame.image.load("picture/character.png")
-char = pygame.transform.scale(char, (200, 150))
+char = pygame.transform.scale(char, (70, 100))
 char_rect = char.get_rect()
 mon = pygame.image.load("picture/monster.png")
 mon = pygame.transform.scale(mon, (100, 100))
@@ -27,6 +27,10 @@ left_pressed = False
 right_pressed = False
 up_pressed = False
 down_pressed = False
+
+monster_mover_time = 0
+move_interval = 150
+
 
 
 running = True
@@ -53,35 +57,35 @@ while running:
             elif event.key == pygame.K_DOWN:
                 down_pressed = False
 
-    x_mon += dx
-    y_mon += dy
+    # if char_rect.colliderect(mon_rect):
+    #     print("va chạm xãy ra!!")
 
-    if x_mon <= 0 or x_mon >= SCREEN_WIDTH - mon_rect.width:
-        dx = -dx
-    if y_mon <= 0 or y_mon >= SCREEN_HEIGHT - mon_rect.height:
-        dy = -dy
 
-    if left_pressed:
-        if x <= 0:
-            x = -speed            
+    monster_mover_time += clock.get_time()
+
+    if monster_mover_time >= move_interval:
+        x_mon += dx
+        y_mon += dy
+
+        if x_mon <= 0 or x_mon >= SCREEN_WIDTH - mon_rect.width:
+            dx = -dx
+        if y_mon <= 0 or y_mon >= SCREEN_HEIGHT - mon_rect.height:
+            dy = -dy
+        monster_mover_time = 0
+
+    if left_pressed and x > 35:            
         x -= speed
-    if right_pressed:
-        if x >= SCREEN_WIDTH:
-            x = SCREEN_WIDTH - speed
+    if right_pressed and x < SCREEN_WIDTH - 35:
         x += speed
-    if up_pressed:
-        if y <= 0:
-            y = 0
+    if up_pressed and y > 50:
         y -= speed
-    if down_pressed:
-        if y >= SCREEN_HEIGHT:
-            y = SCREEN_HEIGHT
+    if down_pressed and y < SCREEN_HEIGHT - 50:
         y += speed
             
 
     screen.fill(WHITE)
     screen.blit(bg, (0, 0))
-    screen.blit(char, (x - char_rect.width // 2, y - char_rect.height // 2))
+    screen.blit(char, (x - char.get_width() // 2, y - char.get_height() // 2))
     screen.blit(mon, (x_mon, y_mon))
     pygame.display.update()
     clock.tick(120)

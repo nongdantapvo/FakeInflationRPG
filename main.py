dd659db53af7c3874f92a26df5267c1fff7a369a
Kim Hieu <kimhieu280@gmail.com>
2024-05-13 10:42:54 +0700

    Update 1

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ import pygame, sys, random
 
 pygame.init()
 screen = pygame.display.set_mode((1000,759))
+pygame.display.set_caption("Fake Inflation RPG")
+
+sub_screen = pygame.Surface((450, 450))
+sub_rect = sub_screen.get_rect(center=screen.get_rect().center)
+sub_screen.fill((255,255,255))
+
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('picture/background.png').convert()
-char_surface = pygame.image.load("picture/character.png").convert_alpha()
+char_speed = 2
+
+bg_surface = pygame.image.load('FakeInflationRPG/picture/background.png').convert()
+battle_surface = pygame.image.load('FakeInflationRPG/picture/background-battle.jfif').convert()
+# battle_surface = pygame.transform.scale(battle_surface, (500,500))
+
+char_surface = pygame.image.load("FakeInflationRPG/picture/character.png").convert_alpha()
+char_surface = pygame.transform.scale(char_surface, (50, 50))
 char_rect = char_surface.get_rect(center = (500, 380))
-pygame.draw.rect(bg_surface, (255,0,0), char_rect, 2)
-mon_surface = pygame.image.load("picture/monster.png").convert_alpha()
+
+mon_surface = pygame.image.load("FakeInflationRPG/picture/monster.png").convert_alpha()
+mon_surface = pygame.transform.scale(mon_surface, (50, 50))
+mon_rect = mon_surface.get_rect(center = (200, 200))
+x_mov_mon = random.choice([-1, 1])
+y_mov_mon = random.choice([-1, 1])
+#time skip collution screen for monster
+pause_time = 0
+pause_duration = 500
+#check key press
+left_press = False
+up_press = False
+right_press = False
+down_press = False
+
 
 active = True
 while active:
@@ -16,100 +41,61 @@ while active:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_press = True
+            elif event.key == pygame.K_RIGHT:
+                right_press = True
+            elif event.key == pygame.K_UP:
+                up_press = True
+            elif event.key == pygame.K_DOWN:
+                down_press = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left_press = False
+            elif event.key == pygame.K_RIGHT:
+                right_press = False
+            elif event.key == pygame.K_UP:
+                up_press = False
+            elif event.key == pygame.K_DOWN:
+                down_press = False
+
     screen.blit(bg_surface,(0,0))
-    screen.blit(char_surface,(0,0))
+    
+    # sub_screen.blit(battle_surface, (0,0))
+    # screen.blit(sub_screen, sub_rect)
+    #character
+    # print(char_rect.centerx)
+    if left_press and char_rect.left > 0:
+        char_rect.centerx -= char_speed
+    if right_press and char_rect.right < 1000:
+        char_rect.centerx  += char_speed
+    if up_press and char_rect.top > 0:
+        char_rect.centery -= char_speed
+    if down_press and char_rect.bottom < 759:
+        char_rect.centery += char_speed
+    screen.blit(char_surface,char_rect)
+    #monster
+    if pause_time > 0:
+        pause_time -= clock.get_time()
+    else:
+        mon_rect.centerx += x_mov_mon
+        mon_rect.centery += y_mov_mon
+        if mon_rect.left < 0 or mon_rect.right > 1000 or mon_rect.top < 0 or mon_rect.bottom > 759:
+            x_mov_mon = -x_mov_mon
+            y_mov_mon = -y_mov_mon
+            mon_rect.move_ip(x_mov_mon * 10, y_mov_mon * 10)
+            pause_time = pause_duration
+            x_mov_mon = random.choice([-1, 1])
+            y_mov_mon = random.randint(-1, 1)
+    screen.blit(mon_surface, mon_rect)
+    if char_rect.colliderect(mon_rect):
+        sub_screen.blit(battle_surface, (0,0))
+        sub_screen.blit(char_surface, char_rect)
+        sub_screen.blit(mon_surface, mon_rect)
+        screen.blit(sub_screen, sub_rect)
+        # #hide monster
+        # x_mov_mon += 1000
+        # y_mov_mon += 1000
     pygame.display.update()
     clock.tick(120)
-
-
-
-
-
-
-# WHITE = (255, 255, 255)
-# SCREEN_WIDTH = 1000
-# SCREEN_HEIGHT = 760
-# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# clock = pygame.time.Clock()
-# pygame.display.set_caption("Inflation_RPG")
-
-# bg = pygame.image.load("picture/background.png")
-# char = pygame.image.load("picture/character.png")
-# char = pygame.transform.scale(char, (70, 100))
-# char_rect = char.get_rect()
-# mon = pygame.image.load("picture/monster.png")
-# mon = pygame.transform.scale(mon, (100, 100))
-# mon_rect = mon.get_rect()
-
-# x, y = SCREEN_WIDTH // 2, SCREEN_HEIGHT //2
-# x_mon, y_mon = SCREEN_WIDTH // 2, SCREEN_HEIGHT //2
-# dx, dy = random.randint(-1, 1), random.randint(-1, 1)
-
-# speed = 2
-# #kiểm tra tình trạng phím
-# left_pressed = False
-# right_pressed = False
-# up_pressed = False
-# down_pressed = False
-
-# monster_mover_time = 0
-# move_interval = 150
-
-
-
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_LEFT:
-#                 left_pressed = True
-#             elif event.key == pygame.K_RIGHT:
-#                 right_pressed = True
-#             elif event.key == pygame.K_UP:
-#                 up_pressed = True
-#             elif event.key == pygame.K_DOWN:
-#                 down_pressed = True
-#         elif event.type == pygame.KEYUP:
-#             if event.key == pygame.K_LEFT:
-#                 left_pressed = False
-#             elif event.key == pygame.K_RIGHT:
-#                 right_pressed = False
-#             elif event.key == pygame.K_UP:
-#                 up_pressed = False
-#             elif event.key == pygame.K_DOWN:
-#                 down_pressed = False
-
-#     # if char_rect.colliderect(mon_rect):
-#     #     print("va chạm xãy ra!!")
-
-
-#     monster_mover_time += clock.get_time()
-
-#     if monster_mover_time >= move_interval:
-#         x_mon += dx
-#         y_mon += dy
-
-#         if x_mon <= 0 or x_mon >= SCREEN_WIDTH - mon_rect.width:
-#             dx = -dx
-#         if y_mon <= 0 or y_mon >= SCREEN_HEIGHT - mon_rect.height:
-#             dy = -dy
-#         monster_mover_time = 0
-
-#     if left_pressed and x > 35:            
-#         x -= speed
-#     if right_pressed and x < SCREEN_WIDTH - 35:
-#         x += speed
-#     if up_pressed and y > 50:
-#         y -= speed
-#     if down_pressed and y < SCREEN_HEIGHT - 50:
-#         y += speed
-            
-
-#     screen.fill(WHITE)
-#     screen.blit(bg, (0, 0))
-#     screen.blit(char, (x - char.get_width() // 2, y - char.get_height() // 2))
-#     screen.blit(mon, (x_mon, y_mon))
-#     pygame.display.update()
-#     clock.tick(120)

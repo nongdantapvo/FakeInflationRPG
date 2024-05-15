6053ccc37b811da86d71734ed1e237980d24ff88
Kim Hieu <kimhieu280@gmail.com>
2024-05-15 13:21:12 +0700

    Update 5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Inflation RPG")
-sub_screen = pygame.Surface((550, 400))
+sub_screen = pygame.Surface((600, 400))
 sub_screen_rect = sub_screen.get_rect(center = screen.get_rect().center)
 # Tải hình ảnh
 background = pygame.image.load('picture/background.png').convert()
@@ -16,7 +16,7 @@ player_image = pygame.transform.scale(player_image, (50, 50))
 enemy_image = pygame.image.load("picture/monster.png").convert_alpha()
 enemy_image = pygame.transform.scale(enemy_image, (50, 50))
 battle_image = pygame.image.load("picture/background-battle.jfif")
-battle_image = pygame.transform.scale(battle_image, (550, 400))
+battle_image = pygame.transform.scale(battle_image, (600, 400))
 # Kiểm tra va chạm nhân vật và kẻ thù
 def check_Collision(player, enemy):
     return player.rect.colliderect(enemy.rect)
@@ -32,6 +32,10 @@ class Player:
     def move(self, dx, dy):
         self.rect.x += dx * self.speed
         self.rect.y += dy * self.speed
+        
+    def draw_battle(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
     
     def draw(self, screen):
         screen.blit(self.image, self.rect.topleft)
@@ -48,6 +52,10 @@ class Enemy:
         self.rect.x += dx * self.speed
         self.rect.y += dy * self.speed
         
+    def draw_battle(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+        
     def draw(self, screen):
         screen.blit(self.image, self.rect.topleft)
 
@@ -59,6 +67,9 @@ enemy = Enemy(300, 300)
 y = random.choice([-1, 1])
 x = random.choice([-1, 1])
 
+# Trạng thái của trò chơi
+game_state = 'play'
+
 # Vòng lặp chính của trò chơi
 clock = pygame.time.Clock()
 running = True
@@ -67,35 +78,42 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()      
-        
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.rect.left > 0:
-        player.move(-1, 0)
-    if keys[pygame.K_RIGHT] and player.rect.right < 800:
-        player.move(1, 0)
-    if keys[pygame.K_UP] and player.rect.top > 0:
-        player.move(0, -1)
-    if keys[pygame.K_DOWN] and player.rect.bottom < 600:
-        player.move(0, 1)
+    if game_state == "play":
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.rect.left > 0:
+            player.move(-1, 0)
+        if keys[pygame.K_RIGHT] and player.rect.right < 800:
+            player.move(1, 0)
+        if keys[pygame.K_UP] and player.rect.top > 0:
+            player.move(0, -1)
+        if keys[pygame.K_DOWN] and player.rect.bottom < 600:
+            player.move(0, 1)
 
-    # Kẻ thù di chuyển
-    if enemy.rect.left < 0 or enemy.rect.right > 800:
-        enemy.rect.x += -x * 10
-        x = random.choice([-1, 1])
-        y = random.choice([-1, 1])
-    if enemy.rect.top < 0 or enemy.rect.bottom > 600:
-        enemy.rect.y += -y * 10
-        y = random.choice([-1, 1])
-        x = random.choice([-1, 1])
-    enemy.move(x, y)
-    if check_Collision(player, enemy):
-        print("chạm")
+        # Kẻ thù di chuyển
+        if enemy.rect.left < 0 or enemy.rect.right > 800:
+            enemy.rect.x += -x * 10
+            x = random.choice([-1, 1])
+            y = random.choice([-1, 1])
+        if enemy.rect.top < 0 or enemy.rect.bottom > 600:
+            enemy.rect.y += -y * 10
+            y = random.choice([-1, 1])
+            x = random.choice([-1, 1])
+        enemy.move(x, y)
+        if check_Collision(player, enemy):
+            game_state = "battle"
     # Vẽ hình nền, nhân vật và kẻ thù
-    screen.blit(background, (0,0))
-    sub_screen.blit(battle_image, (0,0))
-    screen.blit(sub_screen, sub_screen_rect)
-    player.draw(screen)
-    enemy.draw(screen)
+    screen.blit(background, (0,0))        
+    if game_state == "play":
+        player.draw(screen)
+        enemy.draw(screen)
+    elif game_state == "battle":
+        sub_screen.fill((0,0,0))
+        player.draw_battle(125, 175)
+        enemy.draw_battle(450, 175)
+        sub_screen.blit(battle_image, (0,0))
+        player.draw(sub_screen)
+        enemy.draw(sub_screen)
+        screen.blit(sub_screen, sub_screen_rect)
     pygame.display.flip()
     
     # Giới hạn FPS
